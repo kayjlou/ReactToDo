01502c0a0b3b809b5b8263a73d900dc516492287
Katie Lewis <klewi314@gmail.com>
2019-06-05 15:30:03 -0700

    To do list functioning

--- todo/src/App.js
+++ todo/src/App.js
@@ -5,6 +5,7 @@ import List from './components/ToDoList'
 import Form from './components/Form/Form.js'
 import NavBar from './components/NavBar/NavBar'
 import Header from './components/Header/Header'
+import ToDoList from "./components/ToDoList";
 
 // import { AppBar, ToolBar, IconButton, Fab, } from '@material-ui/core'
 // import { MenuIcon, AddIcon, SearchIcon, MoreIcon } from '@material-ui/icons'
@@ -13,14 +14,19 @@ import Header from './components/Header/Header'
 
 class App extends Component {
 
+  state = {
+    todos: ["take out trash"]
+  }
   render() {
+    // let items = this.state.todos.map
 
     return (
       <>
         <NavBar />
+        <br />
+        <Header />
         <br/>
-        <Header/>
-
+        <ToDoList />
         <Form />
       </>
     )
